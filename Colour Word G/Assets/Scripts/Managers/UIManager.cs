f388a3943286337e1fa7a568d6af8e010d5a2a30
Amanda Dzaferovic <s200100@students.aie.edu.au>
2021-12-12 16:40:03 +1030

    Created manager scripts
    Created GameManager and UIManager

---
+++ Colour Word G/Assets/Scripts/Managers/UIManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager Instance;
+
+    private void Awake()
+    {
+        if (Instance)
+            Destroy(Instance);
+
+        Instance = this;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
